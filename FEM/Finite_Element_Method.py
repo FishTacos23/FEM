0714117bc71be5d0bfbda32493f4f1a491e438b4
R2D2\Spencer <spencer.bunnell@gmail.com>
2017-10-24 12:06:20 -0600

    switching computers

--- FEM/Finite_Element_Method.py
+++ FEM/Finite_Element_Method.py
@@ -20,9 +20,11 @@ class FEM(object):
         self.g = g
 
         self.num_basis = n - p - 1
+        self.n_int = p + 1
 
-        self.knot_vector = np.append(np.zeros(p+1), [np.arange(1, n-2*(p+1)), np.zeros(p+1)+(n-2*(p+1))])
-        print self.knot_vector
+        self.knot_vector = np.array(np.zeros(p+1))
+        self.knot_vector = np.insert(self.knot_vector, len(self.knot_vector), np.arange(1, n+1-2*(p+1)))
+        self.knot_vector = np.insert(self.knot_vector, len(self.knot_vector), np.zeros(p+1)+(n+1-2*(p+1)))
 
     def solve(self):
 
@@ -31,39 +33,62 @@ class FEM(object):
         x = []
         u = []
 
-        x_pos = 0.
+        # x_pos = 0.
 
-        for e in xrange(1, self.n + 1):
+        x.append(np.arange(0., 1., 0.000001))
+        n_vals = self.get_n(len(x))
+        ef = self.xga()
 
-            x.append(np.arange(x_pos, x_pos + self.he[e-1], 0.000001))
+        # for e in xrange(1, self.n + 1):
+        #     for j in xrange(1, self.p):
+        #         # local B
+        #         # local N
+        #         # global
+        #
+        #         for a in xrange(1, self.p+1):
+        #             if self._location_matrix(a, e) == 0:
+        #                 break
+        #             for b in xrange(1, self.p+1):
+        #                 if self._location_matrix(b, e) == 0:
 
-            n_val = self.get_n(x[-1])
+            #
+            # u.append(ef[e-1]*n_vals[e-1])
+            #
+            # if e < self.n:
+            #     u_e = n_val[0]*d.item(e-1) + n_val[1]*d.item(e)
+            # else:
+            #     u_e = n_val[0]*d.item(e-1) + n_val[1]*self.g
+            #
+            # u.append(u_e)
+            #
+            # x_pos += self.he[e-1]
 
-            if e < self.n:
-                u_e = n_val[0]*d.item(e-1) + n_val[1]*d.item(e)
-            else:
-                u_e = n_val[0]*d.item(e-1) + n_val[1]*self.g
-
-            u.append(u_e)
-
-            x_pos += self.he[e-1]
-
+        print u
         return u, x, d
 
-    def get_n(self, x):
+    def get_n(self, x_len):
 
-        c = np.arange(-1., 1., 2.0 / float(len(x)))
+        c = np.arange(-1., 1., 2.0 / float(x_len))
 
-        n2 = self.basis(2, c, self.p)
-        n1 = self.basis(1, c, self.p)
+        ne = []
 
-        return [n1, n2]
+        for i in xrange(1, self.n + 1):
+            c_mat = np.asmatrix(self.get_c_e(i))
+            b_vec = np.empty((self.p+1, 1))
+            for j in xrange(1, self.p+1):
+                b_vec[j] = self.basis(j, c, self.p)
+            ne.append(c_mat*b_vec)
+
+        return ne
 
     def xga(self):
-        x_s = np.empty(self.n+1)
-        x_s[0] = 0.
-
-        # for
+        x_a = np.empty(self.n-self.p-1)
+        for a in xrange(1, self.n-self.p):
+            x_a_sum = 0.
+            for j in xrange(a+1, self.p+a+1):
+                x_a_sum += self.knot_vector[j-1]
+            x_a[a-1] = float(x_a_sum)/float(self.p)
+        return x_a
 
     def get_c_e(self, e):
 
@@ -101,21 +126,37 @@ class FEM(object):
         f = np.zeros((self.n, 1), dtype=float)
         x = 0
 
-        for e in xrange(1, self.n+1):
+        for e in xrange(1, self.n + 1):
+            for j in xrange(1, self.p):
+                # local B
+                # local N
+                # global
 
-            for a in xrange(1, 3):
-                i = self._location_matrix(a, e)
-                if i != 0:
-                    for b in xrange(1, 3):
-                        j = self._location_matrix(b, e)
-                        if j != 0:
-                            k[i-1][j-1] += self._kab(a, b, e)
-                    f[i-1] += self._fa(a, e, x)
+                for a in xrange(1, self.p + 1):
+                    i = self._lm(a, e)
+                    if i == 0:
+                        break
+                    for b in xrange(1, self.p + 1):
+                        m = self._lm(b, e)
+                        if m == 0:
+                            k[i - 1][m - 1] += self._kab(a, b, e)
+                    f[i - 1] += self._fa(a, e, x)
 
-            x += self.he[e-1]
-
-        k = np.asmatrix(k)
-        return k.I * f
+                    # for e in xrange(1, self.n+1):
+        #
+        #     for a in xrange(1, 3):
+        #         i = self._location_matrix(a, e)
+        #         if i != 0:
+        #             for b in xrange(1, 3):
+        #                 j = self._location_matrix(b, e)
+        #                 if j != 0:
+        #                     k[i-1][j-1] += self._kab(a, b, e)
+        #             f[i-1] += self._fa(a, e, x)
+        #
+        #     x += self.he[e-1]
+        #
+        # k = np.asmatrix(k)
+        # return k.I * f
 
     def _kab(self, a, b, e):
 
@@ -135,11 +176,8 @@ class FEM(object):
 
         return ((((3 - a) * self.fun(x)) + (a * self.fun(x + self.he[e-2]))) * (self.he[e-1] / 6.0)) + s
 
-    def _location_matrix(self, a, e):
-        if a == 1:
-            return e
+    def _lm(self, a, e):
+        if e + 1 > self.n:
+            return 0
         else:
-            if e + 1 > self.n:
-                return 0
-            else:
-                return e + 1
+            return e + a - 1
