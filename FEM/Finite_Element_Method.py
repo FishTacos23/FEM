05685bb37f62efb751267e4af62b364497655d32
R2D2\Spencer <spencer.bunnell@gmail.com>
2017-10-24 21:57:32 -0600

    Local b, n Global Faulty
    Getting structure of code set up. Local b and n set up. Global set up. There is something faulty somewhere between the three.

--- FEM/Finite_Element_Method.py
+++ FEM/Finite_Element_Method.py
@@ -23,11 +23,7 @@ class FEM(object):
         self.num_basis = n - p - 1
         self.n_int = p + 1
 
-        self.knot_vector = np.array(np.zeros(p+1))
-        self.knot_vector = np.insert(self.knot_vector, len(self.knot_vector), np.arange(1, n))
-        self.knot_vector = np.insert(self.knot_vector, len(self.knot_vector), np.zeros(p+1)+n)
-
-        print self.knot_vector
+        self.knot_vector = self._get_knot_vector()
 
     def solve(self):
 
@@ -40,7 +36,7 @@ class FEM(object):
 
         x.append(np.arange(0., 1., 0.000001))
         n_vals = self.get_n(len(x))
-        ef = self.xga()
+        ef = self._xga()
 
         # for e in xrange(1, self.n + 1):
         #     for j in xrange(1, self.p):
@@ -84,9 +80,78 @@ class FEM(object):
 
         return ne
 
-    def xga(self):
-        x_a = np.empty(self.n-self.p-1)
-        for a in xrange(1, self.n-self.p):
+    def _solve_d(self):
+
+        k = np.zeros((self.n, self.n), dtype=float)
+        f = np.zeros((self.n, 1), dtype=float)
+        xa = self._xga()
+
+        q_s, w_s = self._get_quadratures()
+
+        for e in xrange(1, self.n + 1):
+            for j in xrange(1, self.n_int + 1):
+
+                b_s = []
+                db_s = []
+                ddb_s = []
+
+                for a in xrange(1, self.n_int + 1):
+                    b_s.append(self._local_b(q_s[j-1], a)[0])
+                    db_s.append(self._local_b(q_s[j-1], a)[1])
+                    ddb_s.append(self._local_b(q_s[j-1], a)[2])
+
+                ce = self.get_c_e(e)
+                b_s = np.asarray(b_s)
+                db_s = np.asarray(db_s)
+                ddb_s = np.asarray(ddb_s)
+
+                ne, dne, ddne = self._local_n(q_s[j-1], b_s, db_s, ddb_s, ce)
+
+                dnx, ddnx, jac, x = self._global(ne, dne, ddne, xa[e-1:e+self.p+2])
+
+                for a in xrange(1, self.p + 1):
+                    i = self._lm(a, e)
+                    if i == 0:
+                        break
+                    for b in xrange(1, self.p + 1):
+                        m = self._lm(b, e)
+                        if m == 0:
+                            k[i - 1][m - 1] += self._kab(a, b, e)
+                    f[i - 1] += self._fa(a, e, x[a - 1])
+
+        k = np.asmatrix(k)
+        return k.I * f
+
+    def _get_knot_vector(self):
+        knot_vector = np.array(np.zeros(self.p + 1))
+        knot_vector = np.insert(knot_vector, len(knot_vector), np.arange(1, self.n))
+        knot_vector = np.insert(knot_vector, len(knot_vector), np.zeros(self.p + 1) + self.n)
+
+        return knot_vector
+
+    def _get_quadratures(self):
+
+        if self.n_int == 1:
+            q_points = [0.]
+            weights = [2]
+        elif self.n_int == 2:
+            q_points = [-1./math.sqrt(3.), 1./math.sqrt(3.)]
+            weights = [1, 1]
+        elif self.n_int == 3:
+            q_points = [-math.sqrt(3./5.), 0., math.sqrt(3./5.)]
+            weights = [5./9., 8./9., 5./9.]
+        elif self.n_int == 4:
+            q_points = [-0.8611363115940526, -0.3399810435848563, 0.3399810435848563, 0.8611363115940526]
+            weights = [0.3478548451374538, 0.6521451548625461, 0.6521451548625461, 0.3478548451374538]
+        else:
+            q_points = None
+            weights = None
+
+        return q_points, weights
+
+    def _xga(self):
+        x_a = np.empty(self.n+self.p)
+        for a in xrange(1, self.n+self.p+1):
             x_a_sum = 0.
             for j in xrange(a+1, self.p+a+1):
                 x_a_sum += self.knot_vector[j-1]
@@ -120,94 +185,6 @@ class FEM(object):
                 return np.asarray([[1./6., 0., 0., 0.], [2./3., 2./3., 1./3., 1./6.],
                                    [1./6., 1./3., 2./3., 2./3.], [0., 0., 0., 1./6.]])
 
-    def x_to_c(self, ):
-        pass
-
-    def _solve_d(self):
-
-        k = np.zeros((self.n, self.n), dtype=float)
-        f = np.zeros((self.n, 1), dtype=float)
-        xa = self.xga()
-
-        q_s, w_s = self._get_quadratures()
-
-        for e in xrange(1, self.n + 1):
-            for j in xrange(1, self.n_int + 1):
-
-                b_s = []
-                db_s = []
-                ddb_s = []
-
-                for a in xrange(1, self.n_int + 1):
-                    b_s.append(self._local_b(q_s[j-1], a)[0])
-                    db_s.append(self._local_b(q_s[j-1], a)[1])
-                    ddb_s.append(self._local_b(q_s[j-1], a)[2])
-
-                ce = np.asarray(self.get_c_e(e))
-                b_s = np.asarray(b_s)
-                db_s = np.asarray(db_s)
-                ddb_s = np.asarray(ddb_s)
-
-                ne, dne, ddne = self._local_n(q_s[j-1], b_s, db_s, ddb_s, ce)
-
-                xae = []
-
-                for i in xrange(1, self.p+3):
-                    xae.append(self.knot_vector[i-1])
-
-                x = 0.
-                dx = 0.
-                ddx = 0.
-
-                for a in xrange(1, self.n_int + 1):
-                    x += xne
-
-                for a in xrange(1, self.p + 1):
-                    i = self._lm(a, e)
-                    if i == 0:
-                        break
-                    for b in xrange(1, self.p + 1):
-                        m = self._lm(b, e)
-                        if m == 0:
-                            k[i - 1][m - 1] += self._kab(a, b, e)
-                    f[i - 1] += self._fa(a, e, x)
-
-                    # for e in xrange(1, self.n+1):
-        #
-        #     for a in xrange(1, 3):
-        #         i = self._location_matrix(a, e)
-        #         if i != 0:
-        #             for b in xrange(1, 3):
-        #                 j = self._location_matrix(b, e)
-        #                 if j != 0:
-        #                     k[i-1][j-1] += self._kab(a, b, e)
-        #             f[i-1] += self._fa(a, e, x)
-        #
-        #     x += self.he[e-1]
-        #
-        # k = np.asmatrix(k)
-        # return k.I * f
-
-    def _get_quadratures(self):
-
-        if self.n_int == 1:
-            q_points = [0.]
-            weights = [2]
-        elif self.n_int == 2:
-            q_points = [-1./math.sqrt(3.), 1./math.sqrt(3.)]
-            weights = [1, 1]
-        elif self.n_int == 3:
-            q_points = [-math.sqrt(3./5.), 0., math.sqrt(3./5.)]
-            weights = [5./9., 8./9., 5./9.]
-        elif self.n_int == 4:
-            q_points = [-0.8611363115940526, -0.3399810435848563, 0.3399810435848563, 0.8611363115940526]
-            weights = [0.3478548451374538, 0.6521451548625461, 0.6521451548625461, 0.3478548451374538]
-        else:
-            q_points = None
-            weights = None
-
-        return q_points, weights
-
     def _local_b(self, gc, a):
 
         b = self.basis[0](a, gc, self.p)
@@ -224,14 +201,21 @@ class FEM(object):
 
         return n, dn, ddn
 
-    def _global(self, n, dnc, ddnc, xe):
+    def _global(self, n, dn, ddn, xae):
 
-        dnx = None
-        ddnx = None
-        j = None
-        xc = None
+        xe_c = 0.
+        dxe_c = 0.
+        ddxe_c = 0.
+        for i in xrange(1, self.p+2):
+            xe_c += xae[i-1]*n[i-1]
+            dxe_c += xae[i-1]*dn[i-1]
+            ddxe_c += xae[i-1]*ddn[i-1]
 
-        return dnx, ddnx, j, xc
+        dnx = dn/dxe_c
+        ddnx = (ddn - dnx*ddxe_c)/(dxe_c**2)
+        j = dxe_c
+
+        return dnx, ddnx, j, xe_c
 
     def _kab(self, a, b, e):
 
